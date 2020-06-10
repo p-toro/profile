2ac2cdad353292b84b5e41e24c36a2c0ed49e22b
Kei Watanabe <watanabe@un-t.com>
2020-06-10 10:25:21 +0900

    Change gatsby-image to img tag.

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,10 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import Img from "../images/ico_me.png"
 
 const Header = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "ico_me.png" }) {
-        childImageSharp {
-          fixed(width: 150, height: 150) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `)
   return (
     <header className="header" role="banner">
-      <h1><Img fixed={data.file.childImageSharp.fixed} /><span>p_toro</span></h1>
+      <h1><img src={Img} alt="ピートロのアイコン" /><span>p_toro</span></h1>
     </header>
   )
 }
